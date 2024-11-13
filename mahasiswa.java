df7f5724a7d84fc3a22125b1c9d8b4153d79f168
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-11-14 01:59:46 +0800

    add public class

---
+++ mahasiswa.java
@@ -0,0 +1,28 @@
+public class mahasiswa {
+    private String nama;
+    private String nim;
+    private String jurusan;
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setJurusan(String jurusan) {
+        this.jurusan = jurusan;
+    }
+
+    public String getJurusan() {
+        return jurusan;
+    }
+}
